b375a63b4a188b833dd66585d20841a40806e4b1
Simen Holmestad <simehol@stud.ntnu.no>
2020-10-27 11:09:17 +0100

    Split keras_script.py into two seperate files
    One for creating the model and another file for testing model.
    
    The model is stored in resources/keras_mode.tf

--- src/create_model.py
+++ src/create_model.py
@@ -5,80 +5,10 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-def test_model_on_dataset(model):
-    df = pd.read_csv("../data/balanced_dataset.csv")
-
-    rows = list(df.loc[:, df.columns != "avalanche"].to_numpy())
-
-    for i in range(len(rows)):
-        rows[i] = [float(value) for value in rows[i]]
-
-    labels = df["avalanche"]
-
-    prediction_values_for_avalanche = []
-    prediction_values_for_not_avalanche = []
-
-    for i in range(len(rows)):
-        prediction = model.predict([rows[i]])[0]
-
-        # Round numbers
-        prediction = [round(prediction[0], 2), round(prediction[1], 2)]
-        prediction_value = prediction[0]
-
-        print("True avalanche value: ", labels[i], "- prediction =", prediction, end="")
-
-        failed_value = round(abs(labels[i] - prediction[0]), 2)
-
-        print(", failed by", failed_value)
-
-        if (labels[i] == 1.0):
-            prediction_values_for_avalanche.append(prediction_value)
-        else:
-            prediction_values_for_not_avalanche.append(prediction_value)
-
-    mean_prediction_value_for_avalanche = sum(prediction_values_for_avalanche) / len(prediction_values_for_avalanche)
-    mean_prediction_value_for_not_avalanche = sum(prediction_values_for_not_avalanche) / len(prediction_values_for_not_avalanche)
-
-    print("Mean prediction value for avalanche:", mean_prediction_value_for_avalanche)
-    print("Mean prediction value for not avalanche:", mean_prediction_value_for_not_avalanche)
-
-    # Plot prediction values for avalanche
-    prediction_values_for_avalanche = [round(x, 2) for x in prediction_values_for_avalanche]
-    unique, counts = np.unique(prediction_values_for_avalanche, return_counts=True)
-
-    print("Unique values for avalanche:", unique)
-    print("Count values for avalanche:", counts)
-
-    plt.plot(unique, counts)
-    plt.title("Distribution of probabilities for forecast where avalanche happened")
-    plt.xlabel("Probabilities")
-    plt.ylabel("Number of forecasts")
-    plt.xlim(0, 1)
-    plt.savefig("../plots/probabilities_where_avalanche.png", dpi=300)
-    plt.clf()
-
-    # Plot prediction values for not avalanche
-    prediction_values_for_not_avalanche = [round(x, 2) for x in prediction_values_for_not_avalanche]
-    unique, counts = np.unique(prediction_values_for_not_avalanche, return_counts=True)
-
-    print("Unique values for not avalanche:", unique)
-    print("Count values for not avalanche:", counts)
-
-    plt.plot(unique, counts)
-    plt.title("Distribution of probabilities for forecast where avalanches did not happen")
-    plt.xlabel("Probabilities")
-    plt.ylabel("Number of forecasts")
-    plt.xlim(0, 1)
-    plt.savefig("../plots/probabilities_where_not_avalanche.png", dpi=300)
-    plt.clf()
-
-
 print("Reading dataset")
 
 # Read and shuffle dataset
 df = pd.read_csv("../data/balanced_dataset.csv").sample(frac=1)
-
 
 # Initialize hyperparameters
 NUMBER_OF_FEATURES = len(df.columns) - 1
@@ -159,12 +89,12 @@ results = model.evaluate(x_testing_data, y_testing_data)
 
 print('test loss, test acc:', results)
 
-
 # Print total running time
 end_time = time.time()
 elapsed_time = end_time - start_time
 
 print("Elapsed_time: " + str(round(elapsed_time, 2)) + " seconds")
-print("Testing_model:")
 
-test_model_on_dataset(model)
+model_filename = "../resources/model.tf"
+print("Saving model to", model_filename)
+model.save(model_filename)
