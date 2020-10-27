0cf843fba069d7b8c8250bafde32b72f9e17bd0d
Johannes Åsheim <joh.aasheim@gmail.com>
2020-10-27 09:27:09 +0100

    Mock-data + keras script

--- processer.py
+++ processer.py
@@ -6,13 +6,22 @@ def Process():
     df = pd.read_csv('dataset.csv')
     data_list = []
     dates = []
-    print(df)
-    # Loops thorough the dataframe and change the strings in "Vindstyrke" with numbers. Adds the dates in a list 'dates'.
     
+    #Adds the dates in a list 'dates'.
     for i in df.index:
         dates.append(df["date"][i])
-    print("For loop dates")
 
+    # Makes the list into a set so it only is one of each of the dates.
+    set_date = set(dates)
+
+    # The date in the dataframe is changed with the month
+    for i in set_date:
+        df["date"] = df["date"].replace(i, int(i[5:7]))
+    
+    df["month"] = df["date"]
+    df["month"] = df["month"].replace(12,0)
+
+    # Change the strings in "Vindstyrke" with numbers.
     df["Vindstyrke"] = df.replace(
         {"Vindstyrke": {
             "Stille/svak vind": 0,
@@ -25,29 +34,11 @@ def Process():
             "Storm": 7
             }})
 
-    print("Vindstyrke")
-    print(df["Vindstyrke"])
-
-    # Makes the list into a set so it only is one of each of the dates.
-    set_date = set(dates)
-
-    # The date in the dataframe is changed with the month
-    for i in set_date:
-        df["date"] = df["date"].replace(i, int(i[5:7]))
-    
-    df["month"] = df["date"]
-    df["month"] = df["month"].replace(12,0)
-
-
-    print(df["month"])
-
     # New columns with average temperature
-    col = df.loc[: , ["Temperatur_min","Temperatur_max"]]
-    df['Temperatur_mean'] = col.mean(axis = 1)
+    temp_mean = df.loc[: , ["Temperatur_min","Temperatur_max"]]
+    df['Temperatur_mean'] = temp_mean.mean(axis = 1)
 
-    print("Fixed date and temp")
-
-    # month - kategori
+    # Binary encode month-category
     month_1 = []
     month_2 = []
     month_3 = []
@@ -71,9 +62,8 @@ def Process():
     df["month_2"] = month_2
     df["month_3"] = month_3
 
-    print(df["month_1"],df["month_2"],df["month_3"])
 
-    # day_off
+    # Add day_off of weekend or red_day
     day_off = []
     for ind in df.index:
         if df["weekend"][ind] == 1 or df["red_day"][ind] == 1:
@@ -82,51 +72,27 @@ def Process():
             day_off.append(0)
     df["day_off"] = day_off
 
-
     # Loops thourgh the dataframe and the values is appended in a list of lists. Every list is a row.
     for ind in df.index:
-        data_list.append([ df["month_1"][ind], df["month_2"][ind], df["month_3"][ind], df["day_off"][ind], 
-                        #df["month"][ind], df["weekday"][ind], df["weekend"][ind], df["red_day"][ind], 
-                        df["avalanche"][ind], df["DangerLevel"][ind],
-                          # df["CloudCoverId"][ind], 
-                          df["Nedbor"][ind], df["Vindstyrke"][ind], df["Temperatur_mean"][ind], #df["Temperatur_min"][ind], df["Temperatur_max"][ind],
-                          df["AvalProbabilityId_0"][ind], #df["AvalCauseId_0"][ind], df["DestructiveSizeId_0"][ind], df["AvalTriggerSimpleId_0"][ind],
-                          df["AvalProbabilityId_3"][ind], #df["AvalCauseId_3"][ind], df["DestructiveSizeId_3"][ind], df["AvalTriggerSimpleId_3"][ind],
-                          df["AvalProbabilityId_5"][ind], #df["AvalCauseId_5"][ind], df["DestructiveSizeId_5"][ind], df["AvalTriggerSimpleId_5"][ind],
-                          df["AvalProbabilityId_7"][ind], #df["AvalCauseId_7"][ind], df["DestructiveSizeId_7"][ind], df["AvalTriggerSimpleId_7"][ind],
-                          df["AvalProbabilityId_10"][ind], #df["AvalCauseId_10"][ind], df["DestructiveSizeId_10"][ind], df["AvalTriggerSimpleId_10"][ind],
-                          df["AvalProbabilityId_30"][ind], #f["AvalCauseId_30"][ind], df["DestructiveSizeId_30"][ind], df["AvalTriggerSimpleId_30"][ind],
-                          df["AvalProbabilityId_45"][ind], #df["AvalCauseId_45"][ind], df["DestructiveSizeId_45"][ind], df["AvalTriggerSimpleId_45"][ind],
-                          df["AvalProbabilityId_50"][ind] ]) #, df["AvalCauseId_50"][ind], df["DestructiveSizeId_50"][ind], df["AvalTriggerSimpleId_50"][ind]])
-    print("Looped dataframe and made list of lists")
+        data_list.append([ df["month_1"][ind], df["month_2"][ind], df["month_3"][ind], df["day_off"][ind], df["avalanche"][ind], 
+                        df["DangerLevel"][ind], df["Nedbor"][ind], df["Vindstyrke"][ind], df["Temperatur_mean"][ind], 
+                        df["AvalProbabilityId_0"][ind], df["AvalProbabilityId_3"][ind], df["AvalProbabilityId_5"][ind], 
+                        df["AvalProbabilityId_7"][ind], df["AvalProbabilityId_10"][ind], df["AvalProbabilityId_30"][ind], 
+                        df["AvalProbabilityId_45"][ind], df["AvalProbabilityId_50"][ind] ]) 
+    
+
     # Process the list with data to values between 0-1
     scaler = MinMaxScaler()
     scaler.fit(data_list)
     processed_data = scaler.transform(data_list)
-    print("Normalized")
+    
     # Makes the processed data to a dataframe
     df_processed_data = pd.DataFrame(processed_data)
     
-    df_processed_data.columns = ['month_1', 'month_2', 'month_3', 'day_off', 
-                                'avalanche', 'danger_level', 
-                                'nedbor', 'vind_styrke','temperatur_mean',
-                                'aval_probability_id_0', 'aval_probability_id_3', 'aval_probability_id_5', 'aval_probability_id_7', 'aval_probability_id_10', 'aval_probability_id_30', 
-                                'aval_probability_id_45', 'aval_probability_id_50']
-
-    '''
-    df_processed_data.columns = ['date', 'weekday', 'weekend', 'red_day', 'avalanche', 'danger_level', 'cloud_cover_id', 'nedbor', 'vind_styrke',
-                                 'temperatur_min', 'temperatur_max', 'aval_probability_id_0', 'aval_cause_id_0', 'destructive_size_id_0', 'aval_trigger_simple_id_0',
-                                 'aval_probability_id_3', 'aval_cause_id_3', 'destructive_size_id_3', 'aval_trigger_simple_id_3', 'aval_probability_id_5', 'aval_cause_id_5',
-                                 'destructive_size_id_5', 'aval_trigger_simple_id_5', 'aval_probability_id_7', 'aval_cause_id_7', 'destructive_size_id_7',
-                                 'aval_trigger_simple_id_7', 'aval_probability_id_10', 'aval_cause_id_10', 'destructive_size_id_10', 'aval_trigger_simple_id_10',
-                                 'aval_probability_id_30', 'aval_cause_id_30', 'destructive_size_id_30', 'aval_trigger_simple_id_30', 'aval_probability_id_45',
-                                 'aval_cause_id_45', 'destructive_size_id_45', 'aval_trigger_simple_id_45', 'aval_probability_id_50', 'aval_cause_id_50',
-                                 'destructive_size_id_50', 'aval_trigger_simple_id_50']
-    '''
-
-
-    print("Made dataframe of prosecced data")
-    print(df_processed_data)
+    df_processed_data.columns = ['month_1', 'month_2', 'month_3', 'day_off', 'avalanche', 'danger_level', 'nedbor', 'vind_styrke', 'temperatur_mean',
+                                'aval_probability_id_0', 'aval_probability_id_3', 'aval_probability_id_5', 'aval_probability_id_7', 'aval_probability_id_10', 
+                                'aval_probability_id_30', 'aval_probability_id_45', 'aval_probability_id_50']
+    
     # Makes a csv file of the processed data.
     df_processed_data.to_csv("processed_data.csv", index=False)
 
